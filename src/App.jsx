e9b02955bbdbf594dbd1a95e3c2c0b6bb56f6f0c
jesusGZ <j.gr.zurita@gmail.com>
2023-04-28 09:46:50 -0600

    titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react' 
+import viteLogo from '/favicon.ico'
 import './App.css'
 
 function App() {
@@ -10,11 +9,9 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+         
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <h1>P{ <img src={viteLogo} className="logo"/>}kédex</h1>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
