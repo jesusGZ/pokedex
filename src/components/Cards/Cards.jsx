b9f9f13e6a6b8825d9aed8050e1eb795836531ba
jesusGZ <j.gr.zurita@gmail.com>
2023-05-02 08:24:45 -0600

    card de pokemon

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,47 +1,36 @@
 import React, { useEffect, useState } from 'react';
+
+import { getPokemons } from '../../services/pokeApi';
 import './Cards.scss';
 
-const [pokemons, setPokemons] = useState([])
-
-useEffect(() => { }, []);
-
 const Cards = () => {
+
+	const [pokemons, setPokemons] = useState([])
+
+	async function pokemonsList() {
+		try {
+			const pokemons_list = await getPokemons(100, 151);
+			setPokemons(pokemons_list.results)
+			return;
+		} catch (error) {
+			return;
+		}
+	}
+
+	useEffect(() => { pokemonsList() }, []);
+
 	return (
-		<div className='cards-wapper'>
-			<div className='cards-content'>
-				<div className='cards'>Card</div>
+		<>
+			<div className='cards-wapper'>
+				{pokemons.map((item) => {
+					return (
+						<div className='cards-content' /* onClick={} */>
+							<div className='cards'>{item.name}</div>
+						</div>
+					)
+				})}
 			</div>
-			<div className='cards-content'>
-				<div className='cards'>Card</div>
-			</div>
-			<div className='cards-content'>
-				<div className='cards'>Card</div>
-			</div>
-			<div className='cards-content'>
-				<div className='cards'>Card</div>
-			</div>
-			<div className='cards-content'>
-				<div className='cards'>Card</div>
-			</div>
-			<div className='cards-content'>
-				<div className='cards'>Card</div>
-			</div>
-			<div className='cards-content'>
-				<div className='cards'>Card</div>
-			</div>
-			<div className='cards-content'>
-				<div className='cards'>Card</div>
-			</div>
-			<div className='cards-content'>
-				<div className='cards'>Card</div>
-			</div>
-			<div className='cards-content'>
-				<div className='cards'>Card</div>
-			</div>
-			<div className='cards-content'>
-				<div className='cards'>Card</div>
-			</div>
-		</div>
+		</>
 	);
 };
 
