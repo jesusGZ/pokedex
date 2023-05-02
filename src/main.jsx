192842fc22d6939ba45b731102d5111262c52e27
jesusGZ <j.gr.zurita@gmail.com>
2023-05-02 08:24:28 -0600

    app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import React from 'react';
 
 import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode> <App /> </React.StrictMode>,);
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'),);
