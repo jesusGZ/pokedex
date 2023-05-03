d162b9e73626bcec9e05627da0bc3dfccf789fdf
jesusGZ <j.gr.zurita@gmail.com>
2023-05-03 14:58:19 -0600

    Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import generations from '../../data/generations';
+import CustomNavLink from './CustomNavLink';
 import './Navigation.scss';
 
 const Navigation = () => {
@@ -7,14 +9,11 @@ const Navigation = () => {
 		<div className='navigation'>
 			<h2 className='navigation-tittle'>Selecciona la generación:</h2>
 			<div className='content-navigation-links'>
-				<a className='navigation-links active' href="/gen-i">I</a>
-				<a className='navigation-links' href="/gen-ii">II</a>
-				<a className='navigation-links' href="/gen-iii">III</a>
-				<a className='navigation-links' href="/gen-iv">IV</a>
-				<a className='navigation-links' href="/gen-v">V</a>
-				<a className='navigation-links' href="/gen-vi">VI</a>
-				<a className='navigation-links' href="/gen-vii">VII</a>
-				<a className='navigation-links' href="/gen-viii">VIII</a>
+				{generations.map(({ id, link, text }) => (
+					<CustomNavLink exact key={id} to={link}>
+						{text}
+					</CustomNavLink>
+				))}
 			</div>
 		</div>
 	);
