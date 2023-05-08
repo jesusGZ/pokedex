a893339f5fe4ea2bcda62f63a8a4009a45d79e51
jesusGZ <j.gr.zurita@gmail.com>
2023-05-08 09:31:52 -0600

    cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,49 +1,41 @@
-import React, { useEffect, useState, memo } from 'react';
+import { memo } from 'react';
 
-import { getPokemons, getImageURL, getGenerations } from '../../services/pokeApi';
+import { getImageURL } from '../../services/pokeApi';
 import './Cards.scss';
 
-const Cards = ({ pokemon, onClick }) => {
-
-	/* if (!pokemon) { return null; }
+function Cards({ pokemon, onClick }) {
+	if (!pokemon) { return null; }
 
 	const { name, id, types } = pokemon;
 
 	const imgURL = getImageURL(id),
 		className = types.map(({ type }) => 'type-' + type.name).join(' '),
-		paddedId = '#' + id.toString().padStart(3, '000'); */
-
-	const [pokemons, setPokemons] = useState([])
-
-	async function pokemonsList() {
-		try {
-
-			const generations = await getGenerations(1);
-			console.log(generations);
-			const cantidad = generations.pokemon_species.length
-			const pokemons_list = await getPokemons(cantidad, cantidad);
-			setPokemons(pokemons_list.results)
-			return;
-		} catch (error) {
-			return error;
-		}
-	}
-
-	useEffect(() => { pokemonsList() }, []);
+		paddedId = '#' + id.toString().padStart(3, '000');
 
 	return (
-		<>
-			<div className='cards-wapper'/*  onClick={onClick} */>
-				{pokemons.map((item) => {
-					return (
-						<div className='cards-content' >
-							<div className='cards'>{item.name}</div>
-						</div>
-					)
-				})}
+		<div className="card-container" onClick={onClick}>
+			<div className={`card ${className}`}>
+
+				<div className="bg-pokeball"></div>
+				<span className="pokemon-id">{paddedId}</span>
+
+				<div className="card-title">
+					<h2>{name.replace(/-/g, ' ')}</h2>
+
+					<div className="pokemon-types">
+						{types.map(({ type }) => (
+							<span className="type" key={type.name}>{type.name}</span>
+						))}
+					</div>
+				</div>
+
+				<div className="pokemon-image">
+					<img alt={name} src={imgURL} />
+				</div>
+
 			</div>
-		</>
+		</div>
 	);
-};
+}
 
 export default memo(Cards);
