1c49ff815b3437f3c35af4ec6b3b6e966a974fd1
jesusGZ <j.gr.zurita@gmail.com>
2023-05-08 09:31:45 -0600

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Navigation from './components/Navigation/Navigation';
 import Tittle from './components/Tittle/Tittle';
-import './App.scss';
 import Cards from './components/Cards/Cards';
-
+import generations from './data/generations';
+import './App.scss';
+import PokedexView from './components/PokedexView'
 function App() {
 
   return (
@@ -12,6 +13,18 @@ function App() {
       <div className='content'>
         <Tittle />
         <Navigation />
+
+        <Switch>
+          <Route exact path="/">
+            <Redirect to={generations[0].link} />
+          </Route>
+          {generations.map(({ id, link }) => (
+            <Route key={id} exact path={'/' + link}>
+              <PokedexView generation={id} />
+            </Route>
+          ))}
+        </Switch>
+
         <Cards />
       </div>
     </>
