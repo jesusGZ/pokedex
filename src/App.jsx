8084b3d780d8c06ba324b2dc54c02f43c4eb2d92
jesusGZ <j.gr.zurita@gmail.com>
2023-07-24 15:51:25 -0600

    app asignament

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,37 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Navigation from './components/Navigation/Navigation';
 import Tittle from './components/Tittle/Tittle';
-import Cards from './components/Cards/Cards';
+import DetailsView from './components/DetailsView';
 import generations from './data/generations';
 import './App.scss';
+import PokemonsProvider from './context/pokemonsProvider';
 import PokedexView from './components/PokedexView'
+import withRouter from './router/withRouter';
 function App() {
 
   return (
     <>
-      <div className='content'>
-        <Tittle />
-        <Navigation />
+      <PokemonsProvider>
+        <div className='pokedex-app'>
+          <Tittle />
+          <Navigation />
 
-        <Switch>
-          <Route exact path="/">
-            <Redirect to={generations[0].link} />
-          </Route>
-          {generations.map(({ id, link }) => (
-            <Route key={id} exact path={'/' + link}>
-              <PokedexView generation={id} />
+          <Switch>
+            <Route exact path="/">
+              <Redirect to={generations[0].link} />
             </Route>
-          ))}
-        </Switch>
+            {generations.map(({ id, link }) => (
+              <Route key={id} exact path={'/' + link}>
+                <PokedexView generation={id} />
+              </Route>
+            ))}
+          </Switch>
 
-        <Cards />
-      </div>
+          <DetailsView />
+        </div>
+      </PokemonsProvider>
     </>
   );
 }
 
-export default App;
+export default withRouter(App);
