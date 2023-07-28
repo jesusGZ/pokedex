db5d7199e92631b2dcc3c1b028860040065abc7a
jesusGZ <j.gr.zurita@gmail.com>
2023-07-28 08:39:01 -0600

    navegacion

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,22 +4,15 @@ import generations from '../../data/generations';
 import CustomNavLink from './CustomNavLink';
 import './Navigation.scss';
 
-function Navigation() {
+export default function Navigation() {
 	return (
 		<div className="navigation-container">
-			<h3 className="navigation-title">Select Generation:</h3>
-
+			<h3 className="navigation-title">Selecciona una generación:</h3>
 			<div className="links-container">
-				{
-					generations.map(({ id, link, text }) => (
-						<CustomNavLink exact key={id} to={link}>
-							{text}
-						</CustomNavLink>
-					))
-				}
+				{generations.map(({ id, link, text }) => (
+					<CustomNavLink exact key={id} to={link}>{text}</CustomNavLink>
+				))}
 			</div>
 		</div>
 	);
 }
-
-export default Navigation;
